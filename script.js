aed8ddd85cf6ca92f82cb58c19c30f3ed182be44
Glitch (glitch-hello-website) <none>
2024-03-06 16:51:33 +0000

    🚪💈 Checkpoint ./script.js:65462025/255

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const canvas = document.querySelector('#draw')
 const ctx = canvas.getContext('2d')
 
-ctx.fillstyle = 'red'
+ctx.fillStyle = 'red'
 
-function draw(x, y)
-const circle = new Path2D();
-circle.arc(0, 0, 25, 0, 2 * Math.PI);
-ctx.fill(circle)
+function draw(x, y){
+  const circle = new Path2D();
+  circle.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill(circle)
+}
 
-canvas.addEvenListener('nousedown',(e) => {
-  const
+
+canvas.addEvenListener('mousedown',(e) => {
+  const {
+    clientX, clientY
+  } = e
+  
+  draw(clientX, clientY)
 })
