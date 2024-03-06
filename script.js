2186dc8f364e12eb0e7ad844f5e32d1bdc0f9ee4
Glitch (glitch-hello-website) <none>
2024-03-06 16:43:27 +0000

    🍥🍞 Checkpoint ./index.html:65462025/807 ./style.css:65462025/173 ./script.js:65462025/314

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('#draw')
+const ctx = canvas.getContext('2d')
+
+ctx.fillstyle = 'red'
+
+function draw(x, y)
+const circle = new Path2D();
+circle.arc(0, 0, 25, 0, 2 * Math.PI);
+ctx.fill(circle)
+
+canvas.addEvenListener('nousedown',(e) => {
+  const
+})
